c9e3e2a0b49806f999c980e1f6b9605fda11d5c1
taewaun <taewaunkwon@gmail.com>
2022-11-12 23:39:33 +0900

    chore : app module 에 dataBinding 추가

--- presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
@@ -5,18 +5,16 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.twaun95.presentation.R
+import com.twaun95.presentation.base.BaseActivity
 import com.twaun95.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val viewModel by viewModels<MainActivityViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
     }
 }
