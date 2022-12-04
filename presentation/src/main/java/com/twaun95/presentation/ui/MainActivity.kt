2031f013b776aa47fe9be68385beb183c9141e86
taewaun <taewaunkwon@gmail.com>
2022-12-04 16:40:06 +0900

    feat : KAKAO 지도 View

--- presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
@@ -8,13 +8,25 @@ import com.twaun95.presentation.R
 import com.twaun95.presentation.base.BaseActivity
 import com.twaun95.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import net.daum.mf.map.api.MapView
 import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val viewModel by viewModels<MainActivityViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun initView() {
+        super.initView()
+
+        val mapView = MapView(this)
+        binding.viewMap.addView(mapView)
+    }
+
+    override fun setEvent() {
+        super.setEvent()
+    }
+
+    override fun setObserver() {
+        super.setObserver()
     }
 }
