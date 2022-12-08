f5f47bb8ae05fdd670db2011dacc10281076da6a
taewaun <taewaunkwon@gmail.com>
2022-12-09 00:07:02 +0900

    feat : 현재 위치로 초기화

--- presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.twaun95.presentation.ui
 
-import android.os.Bundle
-import android.util.Log
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.twaun95.presentation.R
 import com.twaun95.presentation.base.BaseActivity
 import com.twaun95.presentation.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 import timber.log.Timber
 
+
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val viewModel by viewModels<MainActivityViewModel>()
@@ -20,6 +22,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initView() {
         super.initView()
 
+        setPermission()
         setMap()
     }
 
@@ -31,32 +34,45 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.setObserver()
     }
 
-    private fun setMap() {
-        val mapView = MapView(this).apply {
-//            setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true)
-            currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-            setCurrentLocationEventListener(object : MapView.CurrentLocationEventListener{
-                override fun onCurrentLocationUpdate(p0: MapView, p1: MapPoint, p2: Float) {
-                    Timber.d("p0: $p0")
-                    Timber.d("p1: $p1")
-                    Timber.d("p2: $p2")
+    private fun setPermission() {
+        val permissionCheck: Int =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
 
-                }
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) { //포그라운드 위치 권한 확인
 
-                override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onCurrentLocationUpdateFailed(p0: MapView?) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
-                    TODO("Not yet implemented")
-                }
-
-            })
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
         }
+
+
+        val permissionCheck2: Int =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            } else {
+                TODO("VERSION.SDK_INT < Q")
+            }
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) { //백그라운드 위치 권한 확인
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                0
+            )
+        }
+    }
+
+    private fun setMap() {
+        val mapView = MapView(this)
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
         binding.viewMap.addView(mapView)
+//        mapView.apply {
+//            setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true)
+//            currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+//        }
     }
 }
