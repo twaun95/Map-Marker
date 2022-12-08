1f551402c17f080b301554bb6bdc85ef83e16718
taewaun <taewaunkwon@gmail.com>
2022-12-08 23:56:30 +0900

    feat : 현재 좌표 얻어오기

--- presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.twaun95.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -8,6 +9,7 @@ import com.twaun95.presentation.R
 import com.twaun95.presentation.base.BaseActivity
 import com.twaun95.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 import timber.log.Timber
 
@@ -18,8 +20,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initView() {
         super.initView()
 
-        val mapView = MapView(this)
-        binding.viewMap.addView(mapView)
+        setMap()
     }
 
     override fun setEvent() {
@@ -28,5 +29,34 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun setObserver() {
         super.setObserver()
+    }
+
+    private fun setMap() {
+        val mapView = MapView(this).apply {
+//            setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true)
+            currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+            setCurrentLocationEventListener(object : MapView.CurrentLocationEventListener{
+                override fun onCurrentLocationUpdate(p0: MapView, p1: MapPoint, p2: Float) {
+                    Timber.d("p0: $p0")
+                    Timber.d("p1: $p1")
+                    Timber.d("p2: $p2")
+
+                }
+
+                override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
+        binding.viewMap.addView(mapView)
     }
 }
