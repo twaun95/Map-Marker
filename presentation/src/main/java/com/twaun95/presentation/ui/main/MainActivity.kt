a03107c01e84bb0ba06651808d8fdcee25539020
taewaun <taewaunkwon@gmail.com>
2022-12-31 20:23:43 +0900

    feat : 키보드 바깥 영역 터치 시 키보드 자판 내리기

--- presentation/src/main/java/com/twaun95/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/main/MainActivity.kt
@@ -2,7 +2,11 @@ package com.twaun95.presentation.ui.main
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.os.Build
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -117,5 +121,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         val markerArray = arrayOf(marker, marker2)
 
         mapView.addPOIItems(markerArray)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
