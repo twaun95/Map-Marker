0e0b1942153dd5d342420769823ccc4bb71bb24f
taewaun <taewaunkwon@gmail.com>
2023-04-08 18:12:12 +0900

    feat : 메인에서 검색 화면으로 이동

--- presentation/src/main/java/com/twaun95/presentation/base/BaseActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/base/BaseActivity.kt
@@ -1,8 +1,13 @@
 package com.twaun95.presentation.base
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -25,8 +30,32 @@ abstract class BaseActivity<VB: ViewDataBinding>(@LayoutRes private val layoutId
         super.onDestroy()
     }
 
-    open fun initView() {}
+    open fun initView() {
+        setPermission()
+    }
     open fun setEvent() {}
     open fun setObserver() {}
 
+
+    private fun setPermission() {
+        val permissionCheck: Int =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) { //포그라운드 위치 권한 확인
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
+
+        val permissionCheck2: Int =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            } else {
+                TODO("VERSION.SDK_INT < Q")
+            }
+
+        if (permissionCheck2 == PackageManager.PERMISSION_DENIED) { //백그라운드 위치 권한 확인
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 0)
+        }
+    }
 }
--- presentation/src/main/java/com/twaun95/presentation/ui/main/MainActivityViewModel.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/main/MainActivityViewModel.kt
@@ -1,10 +1,8 @@
 package com.twaun95.presentation.ui.main
 
-import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.twaun95.presentation.base.BaseViewModel
-import com.twaun95.presentation.model.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -12,37 +10,15 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor() : BaseViewModel() {
 
     private val _isTrackingMode = MutableLiveData<Boolean>(false)
-    val isTrackingMode: LiveData<Boolean>
-        get() = _isTrackingMode
+    val isTrackingMode: LiveData<Boolean> get() = _isTrackingMode
 
-    private val _isMenuBar = MutableLiveData<Boolean>(false)
-    val isMenuBar: LiveData<Boolean>
-        get() = _isMenuBar
-
-    private val _viewState = MutableLiveData<ViewState>(ViewState.MAP)
-    val viewState: LiveData<ViewState>
-        get() = _viewState
-
-    private val _isSearching = MutableLiveData<Boolean>(false)
-    val isSearching: LiveData<Boolean>
-        get() = _isSearching
-
-    val searchText by lazy { MutableLiveData<String>("") }
-
+    val currentLocation by lazy { MutableLiveData<String>("") }
 
     fun trackingModeOnOff() {
         _isTrackingMode.value = _isTrackingMode.value == false
     }
 
-    fun showMenuBar(isMenuBar: Boolean) {
-        _isMenuBar.value = isMenuBar
-    }
-
-    fun setViewState(state: ViewState) {
-        _viewState.value = state
-    }
-
-    fun setSearchingMode(isSearching: Boolean) {
-        _isSearching.value = isSearching
+    fun onSearch(text: String) {
+        currentLocation.postValue(text)
     }
 }
